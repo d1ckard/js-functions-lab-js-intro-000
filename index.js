9defb86469ff3715c571413940c1bf01fe8e1384
RIchard Farrell <richardmfarrell@gmail.com>
2019-07-06 18:21:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(you) {
+function happyHolidaysTo(name) {
   return "Happy Holidays, ${name}!";
 }
+happyHolidaysTo(name);
